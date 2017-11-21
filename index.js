970409e46544727c138923bc943c58b82ad32d18
Darshan Panse <dashan.pierce@homesite.com>
2017-11-21 14:27:01 -0500

    changed color to yellow

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var colors = require('colors');
 
 module.exports = function(msg) {
-    console.log('Hello World !'.red);
+    console.log('Hello World !'.yellow);
     console.log('Your msg is : ' + msg);
 }

d6e9250c455bb87dd799fff21a56c804c338446e
Darshan Panse <dashan.pierce@homesite.com>
2017-11-21 14:43:11 -0500

    changed color to cyan

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var colors = require('colors');
 
 module.exports = function(msg) {
-    console.log('Hello World !'.yellow);
+    console.log('Hello World !'.cyan);
     console.log('Your msg is : ' + msg);
 }

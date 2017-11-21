a8682fd945baff5f34385f552b936d568eca8461
Darshan Panse <dashan.pierce@homesite.com>
2017-11-21 14:13:32 -0500

    added color

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+var colors = require('colors');
+
 module.exports = function(msg) {
-    console.log('Hello World !');
+    console.log('Hello World !'.red);
     console.log('Your msg is : ' + msg);
 }

36938cbb45d4f565005be3ef6737bf5587041c88
Darshan Panse <dashan.pierce@homesite.com>
2017-11-21 14:50:16 -0500

    changed color to magneta

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var colors = require('colors');
 
 module.exports = function(msg) {
-    console.log('Hello World !'.cyan);
+    console.log('Hello World !'.magenta);
     console.log('Your msg is : ' + msg);
 }
